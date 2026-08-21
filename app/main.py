92ca695fdeafad2423c2946f4fa169add952218f
Дмитрий Евтухов <dim.notmid@gmail.com>
2026-08-21 21:44:15 +0300

    мелкое исправление

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from app.config.config import settings
-from contextlib import asynccontextmanager
 from app.adapters.inbound.fastapi.routes import auth, users
-from app.adapters.outbound.persistence.models import Base
-from app.adapters.inbound.fastapi.dependencies import engine
+
 
 app = FastAPI(title=settings.app_name)
 
